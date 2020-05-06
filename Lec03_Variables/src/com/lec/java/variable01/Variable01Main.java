08dffa1b748eff57a47ada3fde6e2b82be4a0131
admin <admin@H36>
2020-05-06 10:26:59 +0900

    2020-05-06

--- Lec03_Variables/src/com/lec/java/variable01/Variable01Main.java
+++ Lec03_Variables/src/com/lec/java/variable01/Variable01Main.java
@@ -1,11 +1,10 @@
 package com.lec.java.variable01;
 /*
 변수 (Variables) :
-	값을 담는 공간.  변수이름(variable name)을 부여해서 관리
+	값을 담는 공간. 변수이름(variable name)을 부여해서 관리
 	
 변수는 사용하기 전에 먼저 '선언(declaration)' 해야 한다
-변수 선언:
-	변수타입 변수이름;
+변수 선언: 변수타입 변수이름;
 
 변수의 초기화 (initialization)
 변수에 최초로 값(value) 을 대입 (assignment) 하는 것.
